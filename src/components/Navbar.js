0975491665445d77fc5378e8b1043a536faa470c
Cole Starnes <colestarnes1@gmail.com>
2021-02-24 05:43:50 -0600

    profile button functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,82 +3,67 @@ import { BsFillPersonFill } from 'react-icons/bs';
 import { FiMail } from 'react-icons/fi';
 import { FaPlus, FaSearch } from 'react-icons/fa';
 import { AiTwotoneBell, AiOutlineSearch } from 'react-icons/ai';
-import './navbar.css';
+import './navbar.css'; 
+
+function profileBtn() {
+  var element = document.getElementById("profileOptions");
+  if (element.classList.contains("hide")) {
+    element.classList.remove("hide");
+  } else {
+    element.classList.add("hide");
+  };
+}
+
+
 function Navbar() {
   return (
-    <nav id="nav-bar">
-      <div className="container">
-        <h2 className="homeBtn">VIZZEY</h2>
-        <div className="search">   
-          <input type="search" placeholder="Search" className="form-control" />
-          <button className="searchBtn"><AiOutlineSearch/></button>
+    <div>
+      <nav id="nav-bar">
+        <div className="container">
+          <h2 className="homeBtn">VIZZEY</h2>
+          <div className="search">
+            <input type="search" placeholder="Search" className="form-control" />
+            <button className="searchBtn"><AiOutlineSearch /></button>
+          </div>
+          <ul className="ugh-buttons">
+            <li className="btn">
+              <button className="icon-btn">
+                <FiMail /></button>
+            </li>
+            <li className="btn">
+              <button className="icon-btn"><FaPlus /></button>
+            </li>
+            <li className="btn">
+              <button className="icon-btn"><AiTwotoneBell /></button>
+            </li>
+            <li className="btn">
+              <button id="profile" className="icon-btn" onClick={profileBtn}><BsFillPersonFill /></button>
+            </li>
+          </ul>
         </div>
-        <ul className="ugh-buttons">
-          <li className="btn">
-            <button className="icon-btn">
-              <FiMail/></button>
-          </li>
-          <li className="btn">
-            <button className="icon-btn"><FaPlus/></button>
-          </li>
-          <li className="btn">
-            <button className="icon-btn"><AiTwotoneBell/></button>
-          </li>
-          <li className="btn">
-            <button className="icon-btn"><BsFillPersonFill/></button>
-          </li>
-        </ul>
+      </nav>
+      <div>
+        <div className="container">
+          <h2 className="homeBtn"></h2>
+          <div className="search">
+
+          </div>
+          <div id="profileOptions" className="container hide">
+            <div className="card" style={{ width: "200px", height: "176px", border: "30px" }}>
+                <button className="topProfileSettings">Profile</button>
+                <button className="profileSettings">Following</button>
+                <button className="profileSettings">Followers</button>
+                <button className="profileSettings">Settings</button>
+                <button className="bottomProfileSettings">Sign Out</button>
+            </div>
+          </div>
+          {/* <li className="btn">
+              <div className="dropdown">lol</div>
+            </li> */}
+        </div>
       </div>
-    </nav>
+    </div>
+
   )
 }
 export default Navbar;
-
-
-    // <section className="search-bar">
-    //   <div className="row">
-    //     <div className="col-lg mx-auto"> 
-    //       <form>
-    //         <div>
-    //           <div className="input-group">
-    //             <div className="homeBtn">
-    //             <h3>VIZZEY</h3> 
-    //             </div> 
-
-    //             <input type="search" placeholder="Search" className="form-control" />
-    //             <button className="searchBtn">
-
-    //               <FaSearch />
-
-    //               </button>
-    //             <div className="input-group-append buttons">
-
-
-    //               <div className="icon">
-    //                 <button className="icon-btn">
-    //                   <h4><FiMail /></h4>
-    //                 </button>
-    //               </div>
-    //               <div className="icon">
-    //                 <button className="icon-btn">
-    //                   <h4><FaPlus /></h4>
-    //                 </button>
-    //               </div>
-    //               <div className="icon">
-    //                 <button className="icon-btn">
-    //                   <h4><AiTwotoneBell /></h4>
-    //                 </button>
-    //               </div>
-    //               <div className="icon">
-    //                 <button className="icon-btn">
-    //                   <h4><BsFillPersonFill /></h4> 
-    //                 </button>  
-    //                 <div className="profileOptions"></div>
-    //               </div>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </form>
-    //     </div>
-    //   </div>
-    // </section>  
